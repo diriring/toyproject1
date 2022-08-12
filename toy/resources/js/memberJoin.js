700c722b4c3aa14e0906c8650ca3c3ee9822cdc0
admin <admin@DA-PC-0439>
2022-08-12 13:48:24 +0900

    비밀번호 검증

--- toy/resources/js/memberJoin.js
+++ toy/resources/js/memberJoin.js
@@ -1,4 +1,54 @@
 $("#joinBtn").on("click", function() {
+	submitAjax();
+});
+
+$("#password").on("blur", function() {
+	chkPW();
+});
+
+$("#rePassword").on("keyup", function() {
+	chkRePW();
+});
+
+//비밀번호 유효성 검증 함수
+function chkPW() {
+
+	let pw = $("#password").val();
+	let num = pw.search(/[0-9]/g);
+	let eng = pw.search(/[a-z]/ig);
+	let spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+	 
+	if(pw.length < 8 || pw.length > 20){
+		$("#pwCheckResult").html("8자리 ~ 20자리 이내로 입력해주세요.");
+		return false;
+	}else if(pw.search(/\s/) != -1){
+		$("#pwCheckResult").html("비밀번호는 공백 없이 입력해주세요.");
+		return false;
+	}else if( (num < 0 && eng < 0) || (eng < 0 && spe < 0) || (spe < 0 && num < 0) ){
+		$("#pwCheckResult").html("영문,숫자, 특수문자 중 2가지 이상을 혼합하여 입력해주세요.");
+		  return false;
+	}else {
+		$("#pwCheckResult").html("사용하실 수 있는 비밀번호 입니다.");
+	}
+
+
+}
+
+//비밀번호 확인 체크
+function chkRePW() {
+	let pw = $("#password").val();
+	let rePw = $("#rePassword").val();
+	
+	$("#rePwCheckResult").html("비밀번호가 일치하지 않습니다.");
+	
+	if(pw == rePw) {
+		$("#rePwCheckResult").html("비밀번호가 일치");
+	}
+	
+}
+
+//input 데이터 ajax로 전송해서 DB INSERT
+function submitAjax() {
 	$.ajax({
 		type: "POST",
 		url: "/member/MemberService.php",
@@ -25,4 +75,4 @@ $("#joinBtn").on("click", function() {
 			alert("에러");
 		}
 	});
-});
+}
--- toy/view/join.php
+++ toy/view/join.php
@@ -19,7 +19,6 @@ include '../temp/bootstrap.php';
 	
 	<div class="container">
 	
-	<form action="joinInsert.php" method="POST">
     	<div class="mb-3">
           <label for="id" class="form-label">ID</label>
           <input type="text" class="form-control" name="id" id="id" placeholder="아이디를 입력해주세요.">
@@ -27,11 +26,13 @@ include '../temp/bootstrap.php';
         <div class="mb-3">
           <label for="password" class="form-label">비밀번호</label>
           <input type="password" class="form-control" name="password" id="password" placeholder="비밀번호를 입력해주세요.">
+          <div id="pwCheckResult"></div>
         </div>
-<!--         <div class="mb-3"> -->
-<!--           <label for="password" class="form-label">비밀번호 확인</label> -->
-<!--           <input type="password" class="form-control" name="id" id="password" placeholder="비밀번호를 다시 입력해주세요."> -->
-<!--         </div> -->
+        <div class="mb-3">
+          <label for="password" class="form-label">비밀번호 확인</label>
+          <input type="password" class="form-control" name="rePassword" id="rePassword" placeholder="비밀번호를 다시 입력해주세요.">
+          <div id="rePwCheckResult"></div>
+        </div>
         <div class="mb-3">
           <label for="name" class="form-label">이름</label>
           <input type="text" class="form-control" name="name" id="name" placeholder="이름을 입력해주세요.">
@@ -49,8 +50,7 @@ include '../temp/bootstrap.php';
         <div class="mb-3">
           <button type="button" id="joinBtn" class="btn btn-primary mb-3">회원가입</button>
         </div>
-    </form>
-    
+
 	</div>
 
 <script src="../resources/js/memberJoin.js"></script>
